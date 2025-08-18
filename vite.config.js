24b9cb1bea73a35ca830f2a762d3cac486afeb76
jacksmedia <alex@j4cks.com>
2025-08-18 13:50:35 -0700

    🛠adjust vite.config to finally be able to see the plugin

--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import netlifyPlugin from '@netlify/vite-plugin'
 
 export default defineConfig({
-  plugins: [react(), netlify()],
-  build: {
-    outDir: 'dist',
-    emptyOutDir: true, // needed for outDir action to work
-  },
-  server: {
-    port: 3000,
-    proxy: {
-      '/test': 'http://localhost:3001',
-      '/health': 'http://localhost:3001',
-      '/query': 'http://localhost:3001',
-      '/api': 'http://localhost:3001' // deployment convention in vercel
-    }
-  }
+  plugins: [react(), netlifyPlugin()],
 })
