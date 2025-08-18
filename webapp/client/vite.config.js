68a30f6229871226703b2e85747af5a62a1276c1
jacksmedia <alex@j4cks.com>
2025-08-18 12:10:15 -0700

    🚧same issue, add custom support package

--- webapp/client/vite.config.js
+++ webapp/client/vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), netlify()],
   build: {
     outDir: 'dist',
     emptyOutDir: true, // needed for outDir action to work
