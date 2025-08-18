0c1c23ccfa9adb47de574baef8eb6cbd9d6ad363
jacksmedia <alex@j4cks.com>
2025-08-18 14:14:13 -0700

    🔨uninstall bogus tripwire netlify/vite-plugin

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import netlify from '@netlify/vite-plugin'
 
 export default defineConfig({
   plugins: [
-    react(),
-    netlify()
+    react()
   ],
 })
