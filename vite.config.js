f0e4b0f3b758bac982daab6e525f6db020025ef6
jacksmedia <alex@j4cks.com>
2025-08-18 13:55:24 -0700

    🚧same issue

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import netlifyPlugin from '@netlify/vite-plugin'
+import netlify from '@netlify/vite-plugin'
 
 export default defineConfig({
-  plugins: [react(), netlifyPlugin()],
+  plugins: [
+    react(),
+    netlify()
+  ],
 })
