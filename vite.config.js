0a4272c3377d1af488f9ad1bbe044febe0dc1662
jacksmedia <alex@j4cks.com>
2025-10-17 14:53:17 -0700

    🔧moves vite config to TLD

---
+++ vite.config.js
@@ -0,0 +1,19 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+export default defineConfig({
+  plugins: [react()],
+  build: {
+    outDir: 'dist',
+    emptyOutDir: true, // needed for outDir action to work
+  },
+  server: {
+    port: 3000,
+    proxy: {
+      '/test': 'http://localhost:3001',
+      '/health': 'http://localhost:3001',
+      '/query': 'http://localhost:3001',
+      '/api': 'http://localhost:3001',
+     }
+  }
+});
