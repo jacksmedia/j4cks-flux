cb1bed9f851719e889bf2bebacea3b88a25bdce4
jacksmedia <alex@j4cks.com>
2025-12-01 17:18:42 -0800

    ✨improve navba & link styling, adds patcher content

--- webapp/client/src/navbar.tsx
+++ webapp/client/src/navbar.tsx
@@ -4,30 +4,33 @@ import { useState } from 'react';
 export default function Navbar() {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
   return (
-    <nav className="bg-slate-800 text-white" style={{position:'fixed', top:0, borderWidth:'2px', borderStyle:'outset', borderColor:'#000'}} >
+    <nav className="bg-slate-800 text-white after:pointer-events-none after:absolute after:inset-x-0 after:bottom-0 after:h-px after:bg-white/10" 
+    style={{position:'fixed', top:0, borderWidth:'2px', borderStyle:'outset', borderColor:'#000'}} >
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-        <div className="flex items-center justify-between h-16">
-          {/* Logo/Brand */}
-          <div className="flex-shrink-0">
+        <div className="relative flex h-16 items-center justify-between">
+          
+          {/* Logo/Brand & Mobile menu button */}
+          <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
             <Link to="/" className="text-xl font-bold">
               AI-Image<br/><span style={{fontSize:'12px'}}>by Jacks Media</span>
             </Link>
           </div>
-
-
-          {/* Mobile menu button */}
-          <div className="">
-            
-            <button
-              type="button"
+           <button
+              type="button" 
               onClick={() => setIsMenuOpen(!isMenuOpen)}
-              className="inline-flex items-center justify-center p-2 ml-4 rounded-md hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white"
+              className="relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-white/5 hover:text-white focus:outline-2 focus:-outline-offset-1 focus:outline-blue-600"
               aria-label="Toggle menu"
             > 
               <svg className="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
                 <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 1h15M1 7h15M1 13h15"/>
               </svg>
             </button>
+
+
+          {/* links on bar */}
+          <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
+            
+           
           </div>
         </div>
       </div>
@@ -36,6 +39,13 @@ export default function Navbar() {
       {isMenuOpen && (
         <div className="">
           <div className="px-2 pt-2 pb-3 space-y-1">
+            <Link
+              to="/"
+              className="block px-3 py-2 rounded-md text-base font-medium hover:bg-gray-700"
+              onClick={() => setIsMenuOpen(false)}
+            >
+              Home
+            </Link>
             <Link
               to="/about"
               className="block px-3 py-2 rounded-md text-base font-medium hover:bg-gray-700"
--- webapp/client/src/pages/patchers.tsx
+++ webapp/client/src/pages/patchers.tsx
@@ -4,6 +4,13 @@ export default function Patchers() {
   return (
   <>
     <Layout title="Binary Patchers" body="Since 2024 I've developed webapps to apply custom changes (patches) to retro game software. Using JavaScript to work with binary files is a real head trip!" />
+    <ul>
+      <li><a href="https://ff6asc.vercel.app/" target='blank'>FF6 "A Soldier's Contingency"</a></li>
+      <li><a href="https://ultima-plus.vercel.app/" target='blank'>FF4 Ultima Plus</a></li>
+      <li><a href="https://tdff5ric.vercel.app/" target='blank'>FF5r-IC</a></li>
+      <li><a href="https://ff4ultima-plus.vercel.app/ulti.html" target='blank'>FF4 Custom Sprites Patcher</a></li>
+      <li><a href="https://rom-multipatcher.vercel.app/" target='blank'>Generic Multi Patcher</a></li>
+    </ul>
   </>
   );
 }
