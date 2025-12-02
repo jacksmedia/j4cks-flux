a7e64c00ad077a923dac39d6232772b4248a97f0
jacksmedia <alex@j4cks.com>
2025-12-01 17:30:15 -0800

    🏁adds guides and about content

--- webapp/client/src/pages/about.tsx
+++ webapp/client/src/pages/about.tsx
@@ -1,5 +1,13 @@
 import Layout from '../pageLayout';
 
 export default function About() {
-  return (<Layout title="About" body="I'm a freelance React and Python developer with a background in secure systems and experience with blockchain and DeFi (web3). I enjoy creating images with generative AI, and hacking Super Nintendo games with open source tools; I also like to document these hobbies." />);
+  return (
+  <>
+    <Layout title="About" body="I'm a freelance React and Python developer with a background in secure systems and experience with blockchain and DeFi (web3). I enjoy creating images with generative AI, and hacking Super Nintendo games with open source tools; I also like to document these hobbies." />
+    <ul>
+      <li><a href="https://www.linkedin.com/in/j4cks/" target='blank'>LinkedIn</a></li>
+      <li><a href="https://linktr.ee/xj4cks" target='blank'>linktree</a></li>
+    </ul>
+  </>
+  );
 }
--- webapp/client/src/pages/guides.tsx
+++ webapp/client/src/pages/guides.tsx
@@ -1,5 +1,16 @@
 import Layout from '../pageLayout';
 
 export default function Guides() {
-  return (<Layout title="Guides" body="I enjoy writing technical guides and documentation, both for my professional skillset and for my hobbies." />);
+  return (
+  <>
+    <Layout title="Guides" body="I enjoy writing technical guides and documentation, both for my professional skillset and for my hobbies." />
+    <ul>
+      <li><a href="https://www.notion.so/xj4cks/Refresh-npm-packages-automatically-ba45c778c0fb4008a4c0073d635e135a" target='blank'>Refresh NPM Packages Automatically</a></li>
+      <li><a href="https://www.notion.so/xj4cks/Absolute-Intro-to-FFV-SNES-Romhacking-b1eb9b57f3224683b191614c7b4825fc" target='blank'>Absolute Intro to FFV SNES Romhacking</a></li>
+      <li><a href="https://www.notion.so/xj4cks/Guide-to-Adding-Portraits-in-FFIV-2056bad31429808fb0a5c0434db0d263" target='blank'>Guide to Adding Portraits in FFIV</a></li>
+      <li><a href="https://www.notion.so/xj4cks/Bulk-IPS-Creator-in-Python-1206bad3142980bf9242db74a137cfc0" target='blank'>Bulk IPS Creator in Python</a></li>
+      <li><a href="https://www.notion.so/xj4cks/Export-Import-Tiles-w-STK-1fd6bad3142980b58106e4f168f391be" target='blank'>Export/Import Tiles w STK</a></li>
+    </ul>  
+  </>
+  );
 }
