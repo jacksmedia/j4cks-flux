fe259ba219a4f9388e5599a2c9205557e7b18bb9
jacksmedia <alex@j4cks.com>
2026-03-03 17:02:03 -0800

    📈adjust bio pages

--- webapp/client/src/pages/about.tsx
+++ webapp/client/src/pages/about.tsx
@@ -3,7 +3,7 @@ import Layout from '../pageLayout';
 export default function About() {
   return (
   <>
-    <Layout title="About" body="I'm a freelance React and Python developer with a background in secure systems and experience with blockchain and DeFi (web3). I enjoy creating images with generative AI, and hacking Super Nintendo games with open source tools; I also like to document these hobbies." />
+    <Layout title="About" body="I'm a software developer with a background in secure systems and a degree in general Psychology. Lately I'm learning how to build with AI model inference providers and LangGraph. I have experience with blockchain and DeFi (web3). I enjoy creating images with generative AI, and hacking Super Nintendo games with open source tools; I also like to document these hobbies." />
     <ul>
       <li><a href="https://www.linkedin.com/in/j4cks/" target='blank'>LinkedIn</a></li>
       <li><a href="https://linktr.ee/xj4cks" target='blank'>linktree</a></li>
